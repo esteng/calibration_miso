7ce07c81edbe1ea7ff96f6b0e127ca2c15c6619c
esteng <elias.stengel@gmail.com>
2021-10-22 08:41:55 -0400

    check in miso scripts

---
+++ slurm_scripts/train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+#SBATCH -o /home/estengel/incremental-function-learning/logs/train.out
+#SBATCH -p brtx6
+#SBATCH --gpus=1
+
+# need CHECKPOINT_DIR, TRAINING_CONFIG, TEST_DATA, FXN set 
+
+./experiments/calflow.sh -a train 
+./experiments/calflow.sh -a eval_fxn
+
