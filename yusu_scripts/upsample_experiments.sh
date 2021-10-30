4c70519831e0c2afbde84049420dc733f0d943c5
esteng <elias.stengel@gmail.com>
2021-10-30 10:48:49 -0400

    do just one upsample seed for now

--- yusu_scripts/upsample_experiments.sh
+++ yusu_scripts/upsample_experiments.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # submit each one; this will take a long time
-for fxn in FindManager Tomorrow DoNotConfirm FenceAttendee PlaceHasFeature 
+SEED=12
+for num in 16 32 64
 do
-    ./submit_amlt_all.sh dangerous ${fxn} transformer_upsample_32
-    ./submit_amlt_all.sh dangerous ${fxn} transformer_upsample_16
-    ./submit_amlt_all.sh dangerous ${fxn} transformer_upsample_64
-done 
+    for fxn in FindManager Tomorrow DoNotConfirm FenceAttendee PlaceHasFeature 
+    do
+        #./submit_amlt_all.sh dangerous_one_seed ${fxn} transformer_upsample_${num}
+        ./submit_amlt_dangerous.sh ${FXN} ${SEED} transformer_upsample_${num} 
+    done 
+done
