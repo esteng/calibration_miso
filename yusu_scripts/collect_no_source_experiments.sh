fb6b5e85e09f63597ee73afd5053ae6518a80d22
esteng <elias.stengel@gmail.com>
2021-10-31 14:49:31 -0400

    add dev

--- yusu_scripts/collect_no_source_experiments.sh
+++ yusu_scripts/collect_no_source_experiments.sh
@@ -13,6 +13,7 @@ do
     python scripts/collect_results.py --model-dir ~/amlt_models/transformer_no_source --data-dir ~/resources/data/smcalflow.agent.data --out-path ${results_dir}/${fxn}_transformer_no_source.csv --fxn ${fxn} --fxn-splits 100 --test 
     for seed in 12 31 64
     do 
+        cp ~/amlt_models/transformer_no_source/${fxn}_${seed}_seed/translate_output/dev_valid.tgt ${results_dir}/${fxn}_${seed}_seed_dev_valid.tgt 
         cp ~/amlt_models/transformer_no_source/${fxn}_${seed}_seed/translate_output/test_valid.tgt ${results_dir}/${fxn}_${seed}_seed_test_valid.tgt 
     done 
 done 
