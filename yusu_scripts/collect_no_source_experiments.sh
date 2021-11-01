ecd99dfbdfa4833edc022c39f2b50e051de776cb
esteng <elias.stengel@gmail.com>
2021-11-01 16:10:29 -0400

    fix paths for collection

--- yusu_scripts/collect_no_source_experiments.sh
+++ yusu_scripts/collect_no_source_experiments.sh
@@ -10,10 +10,10 @@ mkdir -p ${results_dir}
 # submit each one; this will take a long time
 for fxn in FindManager Tomorrow DoNotConfirm FenceAttendee PlaceHasFeature 
 do
-    python scripts/collect_results.py --model-dir ~/amlt_models/transformer_no_source --data-dir ~/resources/data/smcalflow.agent.data --out-path ${results_dir}/${fxn}_transformer_no_source.csv --fxn ${fxn} --fxn-splits 100 --test 
+    #python scripts/collect_results.py --model-dir ~/amlt_models/transformer_no_source --data-dir ~/resources/data/smcalflow.agent.data --out-path ${results_dir}/${fxn}_transformer_no_source.csv --fxn ${fxn} --fxn-splits 100 --test 
     for seed in 12 31 64
     do 
-        cp ~/amlt_models/transformer_no_source/${fxn}_${seed}_seed/translate_output/dev_valid.tgt ${results_dir}/${fxn}_${seed}_seed_dev_valid.tgt 
+        #cp ~/amlt_models/transformer_no_source/${fxn}_${seed}_seed/translate_output/dev_valid.tgt ${results_dir}/${fxn}_${seed}_seed_dev_valid.tgt 
         cp ~/amlt_models/transformer_no_source/${fxn}_${seed}_seed/translate_output/test_valid.tgt ${results_dir}/${fxn}_${seed}_seed_test_valid.tgt 
     done 
 done 
