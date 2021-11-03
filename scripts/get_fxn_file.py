5984b6b073f31ee4fbb0fac4e25d22ce138eddfa
esteng <elias.stengel@gmail.com>
2021-11-03 08:32:55 -0400

    fixing path in generating fxn files

--- scripts/get_fxn_file.py
+++ scripts/get_fxn_file.py
@@ -8,12 +8,9 @@ fxns = ["FindManager", "Tomorrow", "DoNotConfirm", "PlaceHasFeature", "FenceAtte
 
 out_paths = ["smcalflow_samples_curated", "smcalflow_samples_curated", "smcalflow_samples_big", "smcalflow_samples", "smcalflow_samples"]
 
+
 for fxn, path in zip(fxns, out_paths):
     for split in ["test", "dev"]: 
-        out_path = root_path.joinpath(path, fxn) 
-        fxn_source_path = out_path.joinpath(f"fxn_{split}_valid.src_tok") 
-        fxn_target_path = out_path.joinpath(f"fxn_{split}_valid.tgt") 
-
         data_path = root_path.joinpath("smcalflow.agent.data") 
         source_path = data_path.joinpath(f"{split}_valid.src_tok") 
         target_path = data_path.joinpath(f"{split}_valid.tgt") 
@@ -23,18 +20,25 @@ for fxn, path in zip(fxns, out_paths):
             src_lines = src_f.readlines()
             tgt_lines = tgt_f.readlines()
 
-        idxs=[]
-        for i, tl in enumerate(tgt_lines):
-            if fxn in tl.strip().split(" "):
-                idxs.append(i) 
+        for num in [5000, 10000, 20000, 50000, 100000, "max"]: 
+            
+            out_path = root_path.joinpath(path, fxn, f"{num}_100") 
+            fxn_source_path = out_path.joinpath(f"fxn_{split}_valid.src_tok") 
+            fxn_target_path = out_path.joinpath(f"fxn_{split}_valid.tgt") 
 
-        fxn_src_lines = [src_lines[i] for i in idxs]
-        fxn_tgt_lines = [tgt_lines[i] for i in idxs]
 
-        with open(fxn_source_path, "w") as fsrc_f, open(fxn_target_path, "w") as ftgt_f:
-            for sl, tl in zip(fxn_src_lines, fxn_tgt_lines):
-                fsrc_f.write(sl.strip() + "\n") 
-                ftgt_f.write(tl.strip() + "\n") 
+            idxs=[]
+            for i, tl in enumerate(tgt_lines):
+                if fxn in tl.strip().split(" "):
+                    idxs.append(i) 
+
+            fxn_src_lines = [src_lines[i] for i in idxs]
+            fxn_tgt_lines = [tgt_lines[i] for i in idxs]
+
+            with open(fxn_source_path, "w") as fsrc_f, open(fxn_target_path, "w") as ftgt_f:
+                for sl, tl in zip(fxn_src_lines, fxn_tgt_lines):
+                    fsrc_f.write(sl.strip() + "\n") 
+                    ftgt_f.write(tl.strip() + "\n") 
 
 
         
