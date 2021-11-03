5f29683ad5efecce6c38ab5902f0e881cfbf0843
esteng <elias.stengel@gmail.com>
2021-11-03 09:41:39 -0400

    upsample 32x experiments for calflow

--- yusu_scripts/upsample_experiments.sh
+++ yusu_scripts/upsample_experiments.sh
@@ -2,11 +2,12 @@
 
 # submit each one; this will take a long time
 SEED=12
-for num in 16 32 64
-do
+for seed in 12 31 64 
+do 
+    #for num in 16 32 64
+    num=32
     for fxn in FindManager Tomorrow DoNotConfirm FenceAttendee PlaceHasFeature 
     do
-        #./submit_amlt_all.sh dangerous_one_seed ${fxn} transformer_upsample_${num}
         ./submit_amlt_dangerous.sh ${FXN} ${SEED} transformer_upsample_${num} 
     done 
 done
