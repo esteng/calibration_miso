b23589ff7fc46c135f65c5d7f0dc8fc5cc87674c
esteng <elias.stengel@gmail.com>
2022-02-28 10:03:52 -0500

    training and collection scripts

---
+++ yusu_scripts/transformer_experiments_roberta.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# submit each one; this will take a long time
+for fxn in FindManager PlaceHasFeature FenceAttendee Tomorrow DoNotConfirm 
+do
+    ./submit_amlt_all.sh dangerous ${fxn} transformer_roberta
+done 
+
