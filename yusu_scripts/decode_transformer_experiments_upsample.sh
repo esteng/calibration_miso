c2ab7af5b1f94ad68d3ed3b15e9d2390d48a2e06
esteng <elias.stengel@gmail.com>
2022-03-07 17:49:15 -0500

    robert and albert scripts

--- yusu_scripts/decode_transformer_experiments_upsample.sh
+++ yusu_scripts/decode_transformer_experiments_upsample.sh
@@ -2,7 +2,8 @@
 
 
 # submit each one; this will take a long time
-for fxn in FindManager PlaceHasFeature FenceAttendee Tomorrow DoNotConfirm 
+#for fxn in FindManager PlaceHasFeature FenceAttendee Tomorrow DoNotConfirm 
+for fxn in DoNotConfirm 
 do
     ./decode_amlt_all.sh dangerous ${fxn} transformer_upsample_constant
 done 
