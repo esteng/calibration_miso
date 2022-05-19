c260498af82451bf422162a715a465a39b2265e8
esteng <elias.stengel@gmail.com>
2022-05-19 16:52:30 -0400

    remove extraneous files

--- minimal_pair_utils/extract_all_fxn_lines.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FXN=$1
-
-for num in 5000 10000 20000 50000 100000 max
-do
-	python minimal_pair_utils/extract_fxn_lines.py --train-path ~/resources/data/smcalflow_samples_big/${FXN}/${num}_100/ --write-path ~/resources/data/smcalflow_samples_big/${FXN}/${num}_100/ --fxn-of-interest ${FXN} --use-test
-done
