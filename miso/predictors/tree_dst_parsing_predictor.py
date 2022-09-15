e1c1113a25587541323c5da80f1765c42229773e
esteng <elias.stengel@gmail.com>
2022-09-15 10:08:51 -0400

    add dataset readers and predictiors for treedst

---
+++ miso/predictors/tree_dst_parsing_predictor.py
@@ -0,0 +1,37 @@
+
+from miso.nn.beam_search import BeamSearch
+from dataflow.core.lispress import render_compact
+from overrides import overrides
+from typing import List, Iterator, Any
+import numpy
+from contextlib import contextmanager
+import json
+import logging 
+import sys
+import pdb 
+
+import numpy as np 
+from allennlp.predictors.predictor import Predictor
+from allennlp.data import Instance
+from allennlp.common.util import JsonDict
+
+from miso.data.dataset_readers.calflow_parsing.tree_dst_graph import TreeDSTGraph
+from miso.predictors.decomp_parsing_predictor import sanitize
+
+logger = logging.getLogger(__name__) 
+
+
+@Predictor.register("tree_dst_parsing")
+class TreeDSTParsingPredictor(Predictor):
+
+    @overrides
+    def dump_line(self, outputs: JsonDict) -> str:
+        # function hijacked from parent class to return a decomp arborescence instead of printing a line 
+        src_str = " ".join(outputs['src_str'])
+        pred_graph = TreeDSTGraph.from_prediction(src_str,
+                                                outputs['nodes'], 
+                                                outputs['node_indices'], 
+                                                outputs['edge_heads'], 
+                                                outputs['edge_types']) 
+        return pred_graph.tgt_str
+
