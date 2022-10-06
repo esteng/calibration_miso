4916a40326cf05f3a6e3555a3a55dac8b0eb58b0
esteng <elias.stengel@gmail.com>
2022-10-06 14:33:47 -0400

    scripts for running backtranslation

--- calibration_scripts/backtranslation.sh
+++ calibration_scripts/backtranslation.sh
@@ -1,16 +1,18 @@
 #!/bin/bash 
+# REQUIRES
+# $1: calflow_to_text_model 
+# $2: split name 
 
 eval "$(conda shell.bash hook)"
 # instructions for running backtranslation for cycle consistency 
 
 echo "Setting up model and data paths"
 # need a calflow-to-text model and a text-to-calflow model 
-# calflow_to_text_model="/brtx/603-nvme1/estengel/calflow_calibration/benchclamp/calflow_to_text/1.0/t5-base-lm-adapt_calflow_last_agent_all_0.0001/checkpoint-10000/" 
-calflow_to_text_model="/brtx/601-nvme1/estengel/calflow_calibration/benchclamp/lispress_to_text_context/1.0/t5-base-lm-adapt_calflow_last_user_all_0.0001/checkpoint-10000/"
-text_to_calflow_model="/brtx/603-nvme1/estengel/calflow_calibration/tune_roberta_number_tokenize/"
+calflow_to_text_model=$1
+text_to_calflow_model="/brtx/604-nvme1/estengel/calflow_calibration/miso/tune_roberta_tok_fix_benchclamp_data" 
 
 # need calflow files in format for calflow_to_text model
-dev_split="dev_all"
+dev_split=$2
 data_dir="/brtx/601-nvme1/estengel/resources/data/benchclamp/processed/CalFlowV2/"
 dev_input="${data_dir}/${dev_split}.jsonl"
 gold_output_dir="/brtx/601-nvme1/estengel/resources/data/benchclamp/predicted/CalFlowV2/gold/"
@@ -65,17 +67,17 @@ export TEST_DATA=${pred_out_file}
 export FXN="None"
 model_basename=$(basename ${calflow_to_text_model})
 model_parent_basename=$(basename $(dirname ${calflow_to_text_model}))
-pred_miso_out_file="calibration_logs/miso_roundtrip_predicted_from_${model_parent_basename}-${model_basename}.txt"
+pred_miso_out_file="calibration_logs/miso_roundtrip_predicted_from_${model_parent_basename}-${model_basename}_${dev_split}.txt"
 echo "running miso on predicted"
 ./experiments/calflow.sh -a eval_fxn > ${pred_miso_out_file}
 filename=$(basename ${pred_out_file})
-mv ${CHECKPOINT_DIR}/translated_outputs/${filename} ${calflow_to_text_model}/for_roundtrip/predicted_${filename}
+mv ${CHECKPOINT_DIR}/translate_output/${filename}.tgt ${calflow_to_text_model}/for_roundtrip/predicted_${filename}.tgt
 
 # run MISO on predicted 
 export TEST_DATA=${gold_out_file}
 export FXN="None"
-gold_miso_out_file="calibration_logs/miso_roundtrip_gold.txt" 
+gold_miso_out_file="calibration_logs/miso_roundtrip_gold_${dev_split}.txt" 
 echo "Running miso on gold"
 ./experiments/calflow.sh -a eval_fxn > ${gold_miso_out_file}
-mv ${CHECKPOINT_DIR}/translated_outputs/${filename} ${calflow_to_text_model}/for_roundtrip/gold_${filename}
+mv ${CHECKPOINT_DIR}/translate_output/${filename}.tgt ${calflow_to_text_model}/for_roundtrip/gold_${filename}.tgt
 
