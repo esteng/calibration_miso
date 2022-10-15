d42a5bbfa33760b3a778e8e24b99f02451a66c03
esteng <elias.stengel@gmail.com>
2022-10-15 13:22:52 -0500

    added incomplete results

--- hit/scripts/make_qual.py
+++ hit/scripts/make_qual.py
@@ -13,6 +13,15 @@ with open("hit/workers/worked_on_calibration.csv", "w") as f1:
         datapoint["Worker ID"] = worker 
         writer.writerow(datapoint)
 
+round1_turkers = ["A2LMQ4497NMK3S", "A2RBF3IIJP15IH", "AKQAI78JTXXC9"]
+datapoint = {"Worker ID": None, "UPDATE-CompletedCalibrationRound1": 100}
+with open("hit/workers/worked_on_calibration_round1.csv", "w") as f1:
+    writer = csv.DictWriter(f1, fieldnames=datapoint.keys()) 
+    writer.writeheader() 
+    for worker in round1_turkers:
+        datapoint["Worker ID"] = worker 
+        writer.writerow(datapoint)
+
 for worker in all_turkers:
     worker_datapoint = {"Worker ID": worker, f"UPDATE-is_{worker}": 100}
     with open(f"hit/workers/is_{worker}.csv", "w") as f1:
